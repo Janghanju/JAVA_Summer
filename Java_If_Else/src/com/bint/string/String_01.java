8a83c32a60f4ab82bcd176b274c597ac56c0dd76
Janghanju <hanju1215@naver.com>
2017-08-14 02:01:16 +0900

    2017-08-09

---
+++ Java_If_Else/src/com/bint/string/String_01.java
@@ -0,0 +1,21 @@
+package com.bint.string;
+
+public class String_01 {
+
+	public static void main(String[] args) {
+
+		String strNation = "Republic of Korea";
+		System.out.println(strNation);
+		
+		// System.out.println(strNation);
+//		strNation.
+		
+//		System.out.println("Republic of Korea");
+		
+		// 문자열의 길이를 알려주는 method 
+		int intLength = strNation.length();
+		System.out.println(intLength);
+		
+	}
+
+}
